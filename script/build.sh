4bd0e9d2b698847f6734255caef8730846b23878
Cengiz <cengiz@puzly.co>
2026-02-16 06:26:54 +0600

    Build improvements

--- script/build.sh
+++ script/build.sh
@@ -221,15 +221,15 @@ function uninstall_plugin_from_demo()
 
 function install_plugin_to_demo()
 {
-	display_status "Installing Android plugin to demo app"
-	run_gradle_task "installToDemo"
-
 	display_status "Installing iOS plugin to demo app"
 	if [[ -f "$IOS_ARCHIVE" ]]; then
 		$SCRIPT_DIR/install.sh -t $DEMO_DIR -z $IOS_ARCHIVE
 	else
 		display_warning "Cannot install iOS plugin to demo app. '$IOS_ARCHIVE' not found!"
 	fi
+
+	display_status "Installing Android plugin to demo app"
+	run_gradle_task "installToDemo"
 }
 
 
